f834fc953dd94fc9cc528d110ff220b4e2913bc4
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-18 11:25:20 -0700

    Add styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Counter from './components/counter/Counter';
 
+import './app.scss';
+
 class App extends React.Component {
   render() {
     return (
